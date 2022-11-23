2e4ed1b015f79072aa6ec1eb810ea1fc97076c65
Ren Besson <ren.besson@outlook.com>
2022-11-23 00:32:49 -0400

    Script done

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,27 +2,43 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-// Gets the current date and print it to the BOM
-var getDate = dayjs().format("dddd, MMMM D, YYYY");
-$("#currentDay").text(getDate);
-
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  // Gets the current date and print it to the BOM
+  var getDate = dayjs();
+  var currentHour = getDate.format("H");
+  $("#currentDay").text(getDate.format("dddd, MMMM D, YYYY"));
+
+  // Display the hour rows
+  var businessHours = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
+
+  // Prints a hour div with the given hour #
+  var hourRow = function (hour) {
+    return $(`<div id='hour-${hour}' class='row time-block ${
+      hour == currentHour ? "present" : hour < currentHour ? "past" : "future"
+    }'>
+  <div class='col-2 col-md-1 hour text-center py-3'>${hour}:00</div>
+  <textarea id='hour-${hour}-description' class='col-8 col-md-10 description' rows='3'> </textarea>
+  <button id='hour-${hour}-btn' class='btn saveBtn col-2 col-md-1' aria-label='save'>
+    <i class='fas fa-save' aria-hidden='true'></i>
+  </button>
+</div>`);
+  };
+
+  // Iterate through the business hours to append them to the hours container
+  $.each(businessHours, function (index, value) {
+    $("#hours-container").append(hourRow(value));
+  });
+
+  // Event listener to detect any click event on <button> elements inside the element #hours-container
+  $("#hours-container").on("click", "button", function (event) {
+    var id = event.currentTarget.id.slice(0, 7);
+    var value = $(`#${id}-description`).val();
+    localStorage.setItem(id, value);
+  });
+
+  // Reads the localStorage and prints the results on their respective textareas
+  $.each(businessHours, function (index, id) {
+    var savedValue = localStorage.getItem(`hour-${id}`);
+    $(`#hour-${id}-description`).val(savedValue);
+  });
 });
